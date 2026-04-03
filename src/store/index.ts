584e0b8f735ecbb135ba4233ddc02611c826a219
jjcoding-source <jebinjacob62@gmail.com>
2026-04-03 18:56:11 +0530

    feat(store): configure centralized redux store with slices

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,16 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer      from './slices/authSlice'
 import classroomReducer from './slices/classroomSlice'
 import quizReducer      from './slices/quizSlice'
+import dashboardReducer from './slices/dashboardSlice'
+import sessionReducer   from './slices/sessionSlice'
 
 export const store = configureStore({
   reducer: {
     auth:      authReducer,
     classroom: classroomReducer,
     quiz:      quizReducer,
+    dashboard: dashboardReducer,
+    sessions:  sessionReducer,
   },
 })
 
